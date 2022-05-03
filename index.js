50775edcc8ae9c1c76a848a7e719f3169e0aad8c
ricardomouraspl <ricardo.moura@superlogica.com>
2022-05-03 16:51:19 -0300

    tratando erros

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function throwErrorException(error) {
+    throw new Error(chalk.red(error.code, 'file not founded!'));
+}
+
 function readFile(filePath) {
     const enconding = 'utf-8';
-    fs.readFile(filePath, enconding, (_, texto) => {
-        console.log(chalk.green(texto));
+    fs.readFile(filePath, enconding, (error, text) => {
+        if (error) {
+            throwErrorException(error);
+        } else {
+            console.log(chalk.green(text));
+        }
     });
 }
 
-readFile('./arquivos/texto1.md');
+readFile('./arquivos');
