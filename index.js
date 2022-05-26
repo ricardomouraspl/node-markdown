3d7144d8149f08af90ab8b7ffd60eb81c88ee182
ricardomouraspl <ricardo.moura@superlogica.com>
2022-05-26 16:32:55 -0300

    extract link feature

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const fs = require('fs');
 
 const filePath = './arquivos/texto.md';
 
-function throwErrorException(error) {
-    throw new Error(chalk.red(error.code, 'file not founded!'));
-}
+const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
 
-async function readFile(filePath) {
-    const enconding = 'utf-8';
-    try {
-        const text = await fs.promises.readFile(filePath, enconding);
-        console.log(chalk.green(text));
-    } catch (error) {
-        throwErrorException(error);
-    } finally {
-        console.log('process finished');
-    }
+function extractLinks(text) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const extractedLinks = text.match(regex);
+    console.log(extractedLinks);
 }
+extractLinks(text);
 
-readFile(filePath);
+// function throwErrorException(error) {
+//     throw new Error(chalk.red(error.code, 'file not founded!'));
+// }
+
+// async function readFile(filePath) {
+//     const enconding = 'utf-8';
+//     try {
+//         const text = await fs.promises.readFile(filePath, enconding);
+//         console.log(chalk.green(text));
+//     } catch (error) {
+//         throwErrorException(error);
+//     } finally {
+//         console.log('process finished');
+//     }
+// }
+
+// readFile(filePath);
