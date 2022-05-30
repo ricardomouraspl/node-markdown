1e1c4514048067692ef1d9286f0d49b6eb92f0fe
ricardomouraspl <ricardo.moura@superlogica.com>
2022-05-30 08:36:30 -0300

    finalizando captura de links

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
-const filePath = './arquivos/texto.md';
-
-const text = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+const filePath = './arquivos/texto1.md';
 
 function extractLinks(text) {
     const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const extractedLinks = text.match(regex);
-    console.log(extractedLinks);
+    const resultArray = [];
+    let temp;
+
+    while ((temp = regex.exec(text)) !== null) {
+        resultArray.push({
+            [temp[1]]: temp[2]
+        });
+    }
+    return resultArray;
 }
-extractLinks(text);
 
-// function throwErrorException(error) {
-//     throw new Error(chalk.red(error.code, 'file not founded!'));
-// }
+function throwErrorException(error) {
+    throw new Error(chalk.red(error.code, 'file not founded!'));
+}
 
-// async function readFile(filePath) {
-//     const enconding = 'utf-8';
-//     try {
-//         const text = await fs.promises.readFile(filePath, enconding);
-//         console.log(chalk.green(text));
-//     } catch (error) {
-//         throwErrorException(error);
-//     } finally {
-//         console.log('process finished');
-//     }
-// }
+async function readFile(filePath) {
+    const encoding = 'utf-8';
+    try {
+        const text = await fs.promises.readFile(filePath, encoding);
+        // console.log(chalk.green(text));
+        console.log(extractLinks(text));
+    } catch (error) {
+        throwErrorException(error);
+    } finally {
+        console.log('process finished');
+    }
+}
 
-// readFile(filePath);
+readFile(filePath);
