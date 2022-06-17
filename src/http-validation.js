d35786335356faaabc4de3a84c17753dd3a2da05
ricardomouraspl <ricardo.moura@superlogica.com>
2022-06-17 15:33:08 -0300

    status code on the link object

--- src/http-validation.js
+++ src/http-validation.js
@@ -1,21 +1,37 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function handleError(error) {
+    throw new Error(error.message);
+}
+
 async function checkUrlsStatus(urls) {
-    const statusArray = await Promise.all(urls.map(async url => {
-        const res = await fetch(url);
-        return res.status;
-    }));
-    return statusArray;
+    try {
+        const statusArray = await Promise
+            .all(urls
+                .map(async url => {
+                    const res = await fetch(url);
+                    return res.status;
+                }));
+        return statusArray;
+    } catch (error) {
+        handleError(error);
+    }
 }
 
 function createNewLinksArray(rawData) {
-    return rawData.map(linkObject => Object.values(linkObject).join());
+    return rawData
+        .map(linkObject => Object
+                .values(linkObject).join());
 }
 
 async function validateUrls(data) {
     const links = createNewLinksArray(data);
     const statusLinks = await checkUrlsStatus(links);
-    return statusLinks;
+
+    return data.map((object, index) => ({
+        ...object,
+        status: statusLinks[index]
+    }));
 }
 
 module.exports = validateUrls;
